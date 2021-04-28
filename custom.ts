2588b1fc8881b061f195088cf1f0734503729c03
TgJe <51323906+tangjie133@users.noreply.github.com>
2021-04-28 09:03:57 +0800

    MakeCode 项目的初始文件

---
+++ custom.ts
@@ -0,0 +1,51 @@
+
+
+
+enum MyEnum {
+    //% block="Temp"
+    Temp,
+    //% block="Humidity"
+    Humidity
+}
+
+/**
+ * 温度和湿度的图形化
+ */
+//% weight=100 color=#0fbc11 icon="" block="Temp And Humidity"
+namespace tempAndHumidity {
+    let addr = 0x17
+    /**
+     * TODO: 获取温度和湿度
+     * @param e 获取温度和湿度
+     */
+    //% block="read %myStatic"
+    export function foo(myStatic: MyEnum): number {
+        pins.i2cWriteNumber(addr, 0x03, NumberFormat.Int8LE);
+        basic.pause(50);
+        let buffer = pins.i2cReadBuffer(addr, 4);
+        let myData
+        switch (myStatic){
+            case MyEnum.Temp:
+                myData = buffer[0];
+            break;
+            case MyEnum.Humidity:
+                myData = buffer[2];
+            break;
+            default:
+                myData = 0;
+        }
+        return myData;
+    }
+
+    /**
+     * TODO: 获取版本号
+     */
+    //% block="read version"
+    export function version(): string {
+        pins.i2cWriteNumber(addr, 0x00, NumberFormat.Int8LE);
+        basic.pause(50);
+        let buffer = pins.i2cReadBuffer(addr, 3);
+
+        return String.fromCharCode(buffer[0])+String.fromCharCode(buffer[1])+String.fromCharCode(buffer[2])
+    }
+}
